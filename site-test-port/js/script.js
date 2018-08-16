7fc00bab7aad8b1f26c0a55e291bede50ba456d9
Oleg Lirov <martinborman2016@mail.ru>
2018-08-16 16:46:33 +0300

    test-portfolio

---
+++ site-test-port/js/script.js
@@ -0,0 +1,8 @@
+$(function() {
+
+	$('header').mousemove(function(e) {
+		var x = (e.pageX * -1 / 5), y = (e.pageY * -1 / 5);
+		$(this).css('background-position', x + 'px ' + y + 'px');
+	});
+
+});
