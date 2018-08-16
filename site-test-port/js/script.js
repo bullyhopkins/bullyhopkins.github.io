aa716f240a7e791658d9e5b3e21bd518ed63328e
Oleg Lirov <martinborman2016@mail.ru>
2018-08-17 02:04:52 +0300

    two-example
    +Work and zoom

--- site-test-port/js/script.js
+++ site-test-port/js/script.js
@@ -1,5 +1,11 @@
 $(function(){
 
 	$('.info h1').attr('id', 'anim_head');
+	$(".design #zoom_04b").elevateZoom({zoomWindowPosition: 11});
+	$(".design-two #zoom_04b").elevateZoom({zoomWindowPosition: 1});
+
+	$('.design img').on('click', function() {
+		$('.layout-layer').fadeIn(600);
+	});
 
 });
