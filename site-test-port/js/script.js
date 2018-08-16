8157f7d471cab9185ed0b57af1c0dd16cf61d4c0
Oleg Lirov <martinborman2016@mail.ru>
2018-08-16 19:30:31 +0300

    Header
    +Header foto
    +Animate h1

--- site-test-port/js/script.js
+++ site-test-port/js/script.js
@@ -1,8 +1,5 @@
-$(function() {
+$(function(){
 
-	$('header').mousemove(function(e) {
-		var x = (e.pageX * -1 / 5), y = (e.pageY * -1 / 5);
-		$(this).css('background-position', x + 'px ' + y + 'px');
-	});
+	$('.info h1').attr('id', 'anim_head');
 
 });
