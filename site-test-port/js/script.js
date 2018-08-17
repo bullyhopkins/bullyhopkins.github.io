cecdfa76c708b0508daa2fb0c917de7927d14553
Oleg Lirov <martinborman2016@mail.ru>
2018-08-17 05:43:57 +0300

    add examples
    Add examples and animation 4 reasons

--- site-test-port/js/script.js
+++ site-test-port/js/script.js
@@ -4,8 +4,16 @@ $(function(){
 	$(".design #zoom_04b").elevateZoom({zoomWindowPosition: 11});
 	$(".design-two #zoom_04b").elevateZoom({zoomWindowPosition: 1});
 
-	$('.design img').on('click', function() {
-		$('.layout-layer').fadeIn(600);
+	$('.one-list').on('click', function() {
+
+		$('.one-list ul li').each(function(i, el) {
+
+			setTimeout(function() {
+			$(el).addClass("active");
+			}, 500 + (i * 500));
+
+		});
+
 	});
 
 });
