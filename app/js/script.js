bf077044c775e3f7575bb07a511fdfe382ddad47
Oleg Lirov <martinborman2016@mail.ru>
2018-11-02 00:00:12 +0300

    app

---
+++ app/js/script.js
@@ -0,0 +1,13 @@
+$(function () {
+
+	//Cлежение руки за курсором
+	$(document).mousemove(function(e){
+		$('.hand').css({'top': e.pageY - 25, 'left': e.pageX + 3})
+	});
+
+	//Включить свет
+	$('.inp-light').on('click', function () {
+		$('#preloader').fadeOut(1000);
+	});
+
+});
