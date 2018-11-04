23963768f04fbbca282ca9318f1d7cea88bddaa4
Oleg Lirov <martinborman2016@mail.ru>
2018-11-04 18:37:24 +0300

    +1 modal

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,8 @@
 $(function () {
 
+	//Скролл снизу вверх
+	$(document).scrollTop($('body').height());
+
 	//Cлежение руки за курсором
 	$(document).mousemove(function(e){
 		$('.hand').css({'top': e.pageY - 25, 'left': e.pageX + 3})
@@ -10,5 +13,42 @@ $(function () {
 		$('#preloader').fadeOut(1000);
 		$('html, body').css({ 'overflow': 'auto' });
 	});
+
+	//Skills animation
+	// $('.skills-item').hover(function(){
+	// 	$('.skills-item-descr').slideToggle(300);
+	// },
+	// function () {
+	// 	$('.skills-item-descr').slideToggle(300);
+	// });
+
+	//Menu
+	$('.oscar img').on('click', function () {
+		$('.oscar').fadeOut(500);
+		$('.menu').animate({
+			left: 0
+		}, 700);
+	});
+
+	//Sturtup window
+	$('#sturtup-inp').on('click', function (event) {
+		event.preventDefault();
+		$('.sturtup-window').animate({
+			left: 0
+		}, 1500);
+		setTimeout(noScroll, 2000);
+	});
+
+	//Закрытие Sturtup window
+	$('.window-back').on('click', function () {
+		$('.sturtup-window').animate({
+			left: '100%'
+		}, 1500);
+		$('html, body').css({ 'overflow': 'auto' });
+	});
+	
+	function noScroll () {
+		$('html, body').css({ 'overflow': 'hidden' });
+	}
 
 });
