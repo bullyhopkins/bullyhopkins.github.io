333656f666ca2530aa1c55c53127a0a9542e29d1
user <martinborman2016@mail.ru>
2018-12-03 19:31:43 +0300

    new

--- app/js/script.js
+++ app/js/script.js
@@ -1,12 +1,13 @@
 $(function () {
   $('.slider-wrap').slick({
-    dots: true
+    dots: false
   });
 
   $('.comments-slider').slick({
     slidToShow: 1
   });
 
+  //TABS
   $('.tabs-list li').on('click', function () {
     $('.tabs-list li').each(function (inx, elem) {
       $(elem).removeClass('active-tab');
@@ -16,8 +17,27 @@ $(function () {
   });
 
   $('.learn-more-left').on('click', function () {
-    $('.sub-trip-item').slideDown();
+    $('.sub-trip-item').closest('.sub-trip-item').slideDown();
+    $('.learn-more-left').fadeOut(400);
+    $('.learn-more').fadeOut(400);
+    // $(this)
+    //   .addClass('active').siblings().removeClass('active')
+    //   .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
     return false;
+  });
+
+  //Open-mobile-menu
+  $('.open-menu-mobile').on('click', function () {
+    $('.mobile-menu').animate({
+      left: 0
+    }, 500);
+  });
+
+  //Close mob-menu
+  $('.close-mob-menu').on('click', function () {
+    $('.mobile-menu').animate({
+      left: '-100%'
+    }, 500);
   });
 
 });
