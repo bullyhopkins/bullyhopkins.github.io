e188a4a0b295341322e5ddea5cb042b40c1a8e4c
user <martinborman2016@mail.ru>
2018-12-08 17:13:22 +0300

    vog

--- app/js/script.js
+++ app/js/script.js
@@ -1,53 +1,59 @@
 $(function () {
-  $('.slider-wrap').slick({
-    dots: false
+
+  $('.news-slider').slick({
+    dots: true,
+    arrows: true,
+    nextArrow: $('.next'),
+    prevArrow: $('.prev'),
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: false
+          // arrows: false
+        }
+      }
+    ]
   });
 
   $('.comments-slider').slick({
-    slidToShow: 1
-  });
-
-  //TABS
-  $('.tabs-list li').on('click', function () {
-    $(this)
-      .addClass('active-tab').siblings().removeClass('active-tab')
-      .closest('.trip-item').find('.trip-tabs-content').removeClass('active-content').eq($(this).index()).addClass('active-content');
-    // $('.tabs-list li').each(function (inx, elem) {
-    //   $(elem).removeClass('active-tab');
-    // });
-
-    // $('.trip-tabs-content').removeClass('active');
-
-    // var tab = this.attr('data-tab');
-
-    
-    // $(this).addClass('active-tab');
-
-    // $('.'+tab).addClass('active');
-
-    return false;
-  });
-
-  //Slide-trip-item
-  $('.learn-more-left').on('click', function () {
-    $(this).parentsUntil('.trip-item').children('.sub-trip-item').slideDown();
-    $(this).parentsUntil('.trip-item').children('.learn-more').fadeOut(400);
-    $(this).fadeOut(400);
-    return false;
-  });
-
-  //Open-mobile-menu
-  $('.open-menu-mobile').on('click', function () {
-    $('.mobile-menu').animate({
-      left: 0
-    }, 500);
-  });
-
-  //Close mob-menu
-  $('.close-mob-menu').on('click', function () {
-    $('.mobile-menu').animate({
-      left: '-100%'
-    }, 500);
+    dots: true,
+    arrows: true,
+    nextArrow: $('.next-com'),
+    prevArrow: $('.prev-com'),
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    responsive: [
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: false,
+          arrows: false
+        }
+      }
+    ]
   });
 
 });
