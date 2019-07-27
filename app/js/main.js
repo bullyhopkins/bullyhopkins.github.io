261b21c790057029f86673c232203fcf4c653f6c
bully <oleg.lirov@bk.ru>
2019-07-27 15:30:12 +0300

    build +swiper and contacts (no chacks)

--- app/js/main.js
+++ app/js/main.js
@@ -7,85 +7,77 @@ $(function () {
         $(".slide1-h").addClass("fadeInDown");
     }, 600);
 
-    $(".main-menu-list a").on("click", function (e) {
-        let page = $(this).attr("href");
-        $(".page").fadeOut(400);
-        if (page == "#slider1") {
-            $(".page").fadeOut(400);
-            $(page).fadeIn(400);
-            $(".slider1").slick({
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                arrows: false,
-                infinite: true,
-                autoplay: true,
-                autoplaySpeed: 3000
-            });
-        }
-        $(page).fadeIn(400);
-        // alert(pages);
-        return false;
-    });
+    var slider1 = new Swiper(".slider1-wrap", {
+        // loop: true,
+        // slidesPerView: "10%",
+        // loopedSlides: 4,
+        speed: 600,
+        spaceBetween: 0,
+        uniqueNavElements: true,
+        on: {
+            slideChange: function () {
+                if (slider1.activeIndex == 1) {
+                    setInterval(() => {
+                        $(".slide2-sub-h").addClass("fadeInDown");
+                    }, 200);
+                    setInterval(() => {
+                        $(".slide2-h").addClass("fadeInDown");
+                    }, 400);
+                }
+                if (slider1.activeIndex == 2) {
+                    setInterval(() => {
+                        $(".slide3-sub-h").addClass("fadeInDown");
+                    }, 200);
+                    setInterval(() => {
+                        $(".slide3-h").addClass("fadeInDown");
+                    }, 400);
+                }
+                if (slider1.activeIndex == 3) {
+                    setInterval(() => {
+                        $(".slide4-sub-h").addClass("fadeInDown");
+                    }, 200);
+                    setInterval(() => {
+                        $(".slide4-h").addClass("fadeInDown");
+                    }, 400);
+                }
 
-    $(".main-menu-list li").on("click", function () {
-        $(".main-menu-list li").removeClass("active-page");
-        $(this).addClass("active-page");
-        // alert($(this).addClass("active-page"));
-    });
-
-    //Select 
-    $(".select").select2({
-        minimumResultsForSearch: -1,
-        allowClear: true
-    });
-
-    // var slider1 = new Swiper (".slider1", {
-
-    // });
-
-    $(".slider1").slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        infinite: true,
-        autoplay: true,
-        autoplaySpeed: 3000
+                //Slides Navigation
+                $(".num-pages-list li").removeClass("num-active-page");
+                $(".num-pages-list li")[slider1.activeIndex].className += " num-active-page";
+            }
+        },
+        autoplay: {
+            delay: 3000,
+        },
     });
 
     $(".num-pages-list li").on("click", function () {
         $(".num-pages-list li").removeClass("num-active-page");
         $(this).addClass("num-active-page");
-        $(".slider1").slick("slickGoTo", $(this).index());
+        slider1.slideTo($(this).index());
     });
 
-    $('.slider1').on('afterChange', function(event, slick, currentSlide, nextSlide){
-        if (currentSlide == 1) {
-            setInterval(() => {
-                $(".slide2-sub-h").addClass("fadeInDown");
-            }, 200);
-            setInterval(() => {
-                $(".slide2-h").addClass("fadeInDown");
-            }, 400);
+    $(".main-menu-list a").on("click", function (e) {
+        let page = $(this).attr("href");
+        if ($(this).hasClass("active-page")) {
+            return false;
         }
-        if (currentSlide == 2) {
-            setInterval(() => {
-                $(".slide3-sub-h").addClass("fadeInDown");
-            }, 200);
-            setInterval(() => {
-                $(".slide3-h").addClass("fadeInDown");
-            }, 400);
-        }
-        if (currentSlide == 3) {
-            setInterval(() => {
-                $(".slide4-sub-h").addClass("fadeInDown");
-            }, 200);
-            setInterval(() => {
-                $(".slide4-h").addClass("fadeInDown");
-            }, 400);
-        }
-        let pagesNum = $(".num-pages-list li");
-        $(".num-pages-list li").removeClass("num-active-page");
-        pagesNum[currentSlide].className += " num-active-page";
+        $(".page").fadeOut(400);
+        $("." + page).fadeIn(400);
+        $(".main-menu-list a").removeClass("active-page");
+        $(this).addClass("active-page");
+        return false;
+    });
+
+    // $(".main-menu-list li").on("click", function () {
+        
+    //     // alert($(this).addClass("active-page"));
+    // });
+
+    //Select 
+    $(".select").select2({
+        minimumResultsForSearch: -1,
+        allowClear: true
     });
 
     //Contacts
