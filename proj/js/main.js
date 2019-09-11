239422b7e3232ffe0bc609921bc21d3abbdb590b
bully <oleg.lirov@bk.ru>
2019-09-11 20:59:07 +0300

    finish proj

--- proj/js/main.js
+++ proj/js/main.js
@@ -160,7 +160,7 @@ $(function () {
                 $(".now-slide-about").text(this.realIndex + 1);
             },
             init: function () {
-                let allSlides = $(".swiper-slide").length / 2 - 3;
+                let allSlides = $(".swiper-slide").length - 15;
                 $(".all-slides-about").text(allSlides);
             }
         }
@@ -272,86 +272,86 @@ $(function () {
 
     //Pages-parallax
 
-    // function pageParallax () {
-    //     var elem = $('.landing'), //    Контейнер, в котором будем проводить анимацию
-    //         pos = elem.offset(), //    Позиция элемента
-    //         elem_left = pos.left, // Слева
-    //         elem_top = pos.top, // Сверху
-    //         elem_width = elem.width(), // Ширина элемента
-    //         elem_height = elem.height(), // Высота элемента
-    //         x_center, // Координаты центра по оси X
-    //         y_center; // Координаты центра по оси Y
+    {
+        var elem = $('.landing'), //    Контейнер, в котором будем проводить анимацию
+            pos = elem.offset(), //    Позиция элемента
+            elem_left = pos.left, // Слева
+            elem_top = pos.top, // Сверху
+            elem_width = elem.width(), // Ширина элемента
+            elem_height = elem.height(), // Высота элемента
+            x_center, // Координаты центра по оси X
+            y_center; // Координаты центра по оси Y
     
-    //     // Обрабатываем событие перемещения курсора мыши
-    //     $('.landing').mousemove(function (e) {
+        // Обрабатываем событие перемещения курсора мыши
+        $('.landing').mousemove(function (e) {
     
-    //         // Определяем центр элемента (формула легко гуглится)
-    //         x_center = (elem_width / 2) - (e.pageX - elem_left);
-    //         y_center = (elem_height / 2) - (e.pageY - elem_top);
+            // Определяем центр элемента (формула легко гуглится)
+            x_center = (elem_width / 2) - (e.pageX - elem_left);
+            y_center = (elem_height / 2) - (e.pageY - elem_top);
     
-    //         // Проходим по всем блокам с изображениями)
-    //         $('.landing .parallax').each(function () {
+            // Проходим по всем блокам с изображениями)
+            $('.landing .parallax').each(function () {
     
-    //             var speed = $(this).attr('data-speed'), //    Определяем скорость 
-    //                 xPos = Math.round(-1 * x_center / 40 * speed),// Высчитываем позицию по оси X, движения будут инвертированы (-1). Формула подбиралась на глаз
-    //                 yPos = 0;   // Высчитываем позицию по оси Y
+                var speed = $(this).attr('data-speed'), //    Определяем скорость 
+                    xPos = Math.round(-1 * x_center / 40 * speed),// Высчитываем позицию по оси X, движения будут инвертированы (-1). Формула подбиралась на глаз
+                    yPos = 0;   // Высчитываем позицию по оси Y
     
-    //             // Перемещение по оси Y делаем до определенной точки, потом перемещение останавливаем
-    //             // if (yPos < 0)
-    //             //     yPos = -2 * speed;
+                // Перемещение по оси Y делаем до определенной точки, потом перемещение останавливаем
+                // if (yPos < 0)
+                //     yPos = -2 * speed;
     
-    //             // Непосредственно перенос      
-    //             $(this).css('transform', 'translate3d(' + xPos + 'px, ' + yPos + 'px, 0px)');
+                // Непосредственно перенос      
+                $(this).css('transform', 'translate3d(' + xPos + 'px, ' + yPos + 'px, 0px)');
     
-    //         });
-    //     });
+            });
+        });
     
-    //     $('.site').mousemove(function (e) {
+        $('.site').mousemove(function (e) {
     
-    //         // Определяем центр элемента (формула легко гуглится)
-    //         x_center = (elem_width / 2) - (e.pageX - elem_left);
-    //         y_center = (elem_height / 2) - (e.pageY - elem_top);
+            // Определяем центр элемента (формула легко гуглится)
+            x_center = (elem_width / 2) - (e.pageX - elem_left);
+            y_center = (elem_height / 2) - (e.pageY - elem_top);
     
-    //         // Проходим по всем блокам с изображениями)
-    //         $('.site .parallax').each(function () {
+            // Проходим по всем блокам с изображениями)
+            $('.site .parallax').each(function () {
     
-    //             var speed = $(this).attr('data-speed'), //    Определяем скорость 
-    //                 xPos = Math.round(-1 * x_center / 40 * speed),// Высчитываем позицию по оси X, движения будут инвертированы (-1). Формула подбиралась на глаз
-    //                 yPos = 0;   // Высчитываем позицию по оси Y
+                var speed = $(this).attr('data-speed'), //    Определяем скорость 
+                    xPos = Math.round(-1 * x_center / 40 * speed),// Высчитываем позицию по оси X, движения будут инвертированы (-1). Формула подбиралась на глаз
+                    yPos = 0;   // Высчитываем позицию по оси Y
     
-    //             // Перемещение по оси Y делаем до определенной точки, потом перемещение останавливаем
-    //             // if (yPos < 0)
-    //             //     yPos = -2 * speed;
+                // Перемещение по оси Y делаем до определенной точки, потом перемещение останавливаем
+                // if (yPos < 0)
+                //     yPos = -2 * speed;
     
-    //             // Непосредственно перенос      
-    //             $(this).css('transform', 'translate3d(' + xPos + 'px, ' + yPos + 'px, 0px)');
+                // Непосредственно перенос      
+                $(this).css('transform', 'translate3d(' + xPos + 'px, ' + yPos + 'px, 0px)');
     
-    //         });
-    //     });
+            });
+        });
     
-    //     $('.shop').mousemove(function (e) {
+        $('.shop').mousemove(function (e) {
     
-    //         // Определяем центр элемента (формула легко гуглится)
-    //         x_center = (elem_width / 2) - (e.pageX - elem_left);
-    //         y_center = (elem_height / 2) - (e.pageY - elem_top);
+            // Определяем центр элемента (формула легко гуглится)
+            x_center = (elem_width / 2) - (e.pageX - elem_left);
+            y_center = (elem_height / 2) - (e.pageY - elem_top);
     
-    //         // Проходим по всем блокам с изображениями)
-    //         $('.shop .parallax').each(function () {
+            // Проходим по всем блокам с изображениями)
+            $('.shop .parallax').each(function () {
     
-    //             var speed = $(this).attr('data-speed'), //    Определяем скорость 
-    //                 xPos = Math.round(-1 * x_center / 40 * speed),// Высчитываем позицию по оси X, движения будут инвертированы (-1). Формула подбиралась на глаз
-    //                 yPos = 0;   // Высчитываем позицию по оси Y
+                var speed = $(this).attr('data-speed'), //    Определяем скорость 
+                    xPos = Math.round(-1 * x_center / 40 * speed),// Высчитываем позицию по оси X, движения будут инвертированы (-1). Формула подбиралась на глаз
+                    yPos = 0;   // Высчитываем позицию по оси Y
     
-    //             // Перемещение по оси Y делаем до определенной точки, потом перемещение останавливаем
-    //             // if (yPos < 0)
-    //             //     yPos = -2 * speed;
+                // Перемещение по оси Y делаем до определенной точки, потом перемещение останавливаем
+                // if (yPos < 0)
+                //     yPos = -2 * speed;
     
-    //             // Непосредственно перенос      
-    //             $(this).css('transform', 'translate3d(' + xPos + 'px, ' + yPos + 'px, 0px)');
+                // Непосредственно перенос      
+                $(this).css('transform', 'translate3d(' + xPos + 'px, ' + yPos + 'px, 0px)');
     
-    //         });
-    //     });
-    // }
+            });
+        });
+    }
 
     // $(".screen-slider").slick({
     //     slidesToShow: 1
